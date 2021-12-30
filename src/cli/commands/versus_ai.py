e041b84cfc611e2f6d870248e881cb2ec088735c
xnyGitHub <32583691+xnyGitHub@users.noreply.github.com>
2021-12-30 22:15:51 +0000

    removed excess ai,training & two-player cli commands

--- src/cli/commands/versus_ai.py
+++
@@ -1,9 +0,0 @@
-"""Click interface for two player mode
-User gets here by entering: chess ai [OPTIONAL] COMMANDS """
-import click
-
-
-@click.command()
-def ai_mode():
-    """Play chess against an ai"""
-    click.echo("You chose ai timed")
--- src/cli/main.py
+++ src/cli/main.py
@@ -1,11 +1,6 @@
 """Main entry point for cli"""
 import click
-
-
-from src.cli.commands.two_player_mode import two_player
-from src.cli.commands.versus_ai import ai_mode
 from src.cli.commands.training_mode import training_mode
-from src.cli.commands.puzzles import puzzle_mode
 
 
 @click.group()
@@ -13,7 +8,4 @@ def menu():
     """Entry point for click cli"""
 
 
-menu.add_command(ai_mode)
-menu.add_command(two_player)
 menu.add_command(training_mode)
-menu.add_command(puzzle_mode)
