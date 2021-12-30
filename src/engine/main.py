a740eac3911ad27f1648858d8310b6ac5db058d0
xnyGitHub <32583691+xnyGitHub@users.noreply.github.com>
2021-12-30 22:20:07 +0000

    KeyboardAndMouse import changed to Controller

--- src/engine/main.py
+++ src/engine/main.py
@@ -3,14 +3,14 @@
 from src.engine.event_manager import EventManager
 from src.engine.model import GameEngine
 from src.engine.view import PygameView
-from src.engine.controller import KeyboardAndMouse
+from src.engine.controller import Controller
 
 
 def run():
     """Main Entry point"""
     ev_manager = EventManager()
     gamemodel = GameEngine(ev_manager)
-    keyboard = KeyboardAndMouse(ev_manager, gamemodel)
+    keyboard = Controller(ev_manager, gamemodel)
     graphics = PygameView(ev_manager, gamemodel)
     gamemodel.run()
 
