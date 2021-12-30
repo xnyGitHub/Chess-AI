29ec02000ba3d06d1e55672f8aa0523ed3306466
xnyGitHub <32583691+xnyGitHub@users.noreply.github.com>
2021-12-30 22:40:37 +0000

    deleted test_move.py and moves its tests to test_model.py

--- tests/engine/test_model.py
+++ tests/engine/test_model.py
@@ -1,6 +1,6 @@
 """Test class for src/engine/board.py"""
 from unittest.mock import Mock, patch
-from src.engine.model import GameEngine
+from src.engine.model import GameEngine, Move, Capture
 from src.engine.event_manager import EventManager, ClickEvent
 from tests.helpers.general_helpers import mock_print, unmock_print
 
@@ -158,3 +158,57 @@ class TestClickEvents:
         assert game_engine.square_selected == ()
         assert game_engine.player_clicks == []
         assert game_engine.most_recent_valid_move_click == []
+
+
+class TestMove:
+    """Test class for testing Moves"""
+
+    def test_class_move_repr(self):
+        """Test that __repr__ works as intended"""
+        start_square = (0, 0)
+        end_square = (1, 1)
+        move_type = "Normal"
+        new_move = Move(start_square, end_square)
+
+        expected_output = """self.start_square=(0, 0)
+self.end_square=(1, 1)
+self.movetype='Move'
+"""
+        actual_output = new_move.__repr__()
+        assert actual_output == expected_output
+
+    def test_class_move_instance_of_capture(self):
+        """Test that capture class inherits move class works as intended"""
+        start_square = (0, 0)
+        end_square = (1, 1)
+        new_move = Move(start_square, end_square)
+
+        expected_output = False
+        result = isinstance(new_move, Capture)
+        assert result == expected_output
+
+    def test_class_capture_instance_of_move(self):
+        """Test that capture class is NOT an instance of move class"""
+        board = [["--" for _ in range(8)] for _ in range(8)]
+        white_pieces = ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        black_pieces = ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bB"]
+
+        for column in range(8):
+            board[0][column] = white_pieces[column]
+            board[1][column] = "wP"
+            board[6][column] = "bP"
+            board[7][column] = black_pieces[column]
+
+        board[6][0] = "--"
+
+        start_square = (7, 0)
+        end_square = (1, 0)
+        new_move = Capture(start_square, end_square, board)
+
+        expected_output = """self.start_square=(7, 0)
+self.end_square=(1, 0)
+self.movetype='Capture'
+self.piece_moved='bR'
+self.piece_captured='wP'
+"""
+        assert new_move.__repr__() == expected_output
