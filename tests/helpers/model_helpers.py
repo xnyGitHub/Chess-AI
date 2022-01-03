606543514c96398dc949c18cfce96a362e11d0b8
xnyGitHub <32583691+xnyGitHub@users.noreply.github.com>
2022-01-03 15:52:47 +0000

    added helper modules

---
+++ tests/helpers/model_helpers.py
@@ -0,0 +1,40 @@
+"""Model helpers for tests/engine/test_model.py"""
+
+
+def game_engine_repr_output():
+    """Expected GameEngine__repr__() output"""
+    return """GameSate(
+['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
+['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP']
+['--', '--', '--', '--', '--', '--', '--', '--']
+['--', '--', '--', '--', '--', '--', '--', '--']
+['--', '--', '--', '--', '--', '--', '--', '--']
+['--', '--', '--', '--', '--', '--', '--', '--']
+['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP']
+['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bB']
+)"""
+
+
+def game_engine_str_output():
+    """Expected GameEngine__str__() output"""
+    return """wR wN wB wQ wK wB wN wR
+wP wP wP wP wP wP wP wP
+-- -- -- -- -- -- -- --
+-- -- -- -- -- -- -- --
+-- -- -- -- -- -- -- --
+-- -- -- -- -- -- -- --
+bP bP bP bP bP bP bP bP
+bR bN bB bQ bK bB bN bB"""
+
+
+def game_engine_print_output():
+    """Expected print(GameEngine) output"""
+    return """wR wN wB wQ wK wB wN wR
+wP wP wP wP wP wP wP wP
+-- -- -- -- -- -- -- --
+-- -- -- -- -- -- -- --
+-- -- -- -- -- -- -- --
+-- -- -- -- -- -- -- --
+bP bP bP bP bP bP bP bP
+bR bN bB bQ bK bB bN bB
+"""
