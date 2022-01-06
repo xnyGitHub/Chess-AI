c2e0891bb2defdc43ee096dd2539fce7b3b186a7
xnyGitHub <32583691+xnyGitHub@users.noreply.github.com>
2022-01-06 19:37:06 +0000

    refactored test_main to check if GameEngine.run() and PyGameView.initialise() are called

--- tests/engine/test_main.py
+++ tests/engine/test_main.py
@@ -6,16 +6,10 @@ from src.engine.main import run
 class TestMain:
     """Test main module that starts the engine"""
 
-    @mock.patch("src.engine.main.EventManager")
-    @mock.patch("src.engine.main.GameEngine")
-    @mock.patch("src.engine.main.PygameView")
-    @mock.patch("src.engine.main.Controller")
-    def test_main(
-        self, mock_controller, mock_pygame_view, mock_game_engine, mock_event_manager
-    ):
+    @mock.patch("src.engine.view.PygameView.initialise")
+    @mock.patch("src.engine.model.GameEngine.run")
+    def test_main(self, mock_game_engine_run, mock_view_initialise):
         "Test the entry point to the chess engine"
         run()
-        mock_event_manager.assert_called()
-        mock_game_engine.assert_called()
-        mock_controller.assert_called()
-        mock_pygame_view.assert_called()
+        mock_view_initialise.assert_called()
+        mock_game_engine_run.assert_called()
